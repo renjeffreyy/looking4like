801b79cbda23f404e901062d11ec769d4a91d10e
Jeffrey Ren <renjeffreyy@gmail.com>
2024-10-01 13:38:39 -0700

    created project and updated readme

---
+++ README.md
@@ -0,0 +1,26 @@
+To install dependencies:
+```sh
+bun install
+```
+
+To run:
+```sh
+bun run dev
+```
+
+open http://localhost:3000
+
+
+# Looking4Like
+A dating app that will only match you with myself
+
+
+## stack
+
+- bun
+- vite
+- hono
+- remix
+- mySQL
+- Drizzle
+- tailwind
---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import { Hono } from 'hono'
+import { serveStatic } from 'hono/bun'
+
+const app = new Hono()
+
+app.use('/static/*', serveStatic({ root: './' }))
+app.use('/favicon.ico', serveStatic({ path: './favicon.ico' }))
+app.get('/', (c) => c.text('You can access: /static/hello.txt'))
+app.get('*', serveStatic({ path: './static/fallback.txt' }))
+
+export default {
+  port: 8787,
+  fetch: app.fetch
+}
